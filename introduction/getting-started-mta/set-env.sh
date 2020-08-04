f5f0a532024762e97b00c1f08d0e6e7d38c150a6
Miguel P.C <mperezco@redhat.com>
2020-08-04 15:24:51 +0200

    Added deploy script

--- introduction/getting-started-mta/set-env.sh
+++ introduction/getting-started-mta/set-env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This is the script to put provisioning things in, this runs in the foreground
 set -o pipefail 
-oc create -f /var/tmp/web-template-empty-dir-executor.json
+
+/var/tmp/deploy.sh /var/tmp/deployment.properties
 echo "Migration Toolkit for Applications Tutorial Ready!"
