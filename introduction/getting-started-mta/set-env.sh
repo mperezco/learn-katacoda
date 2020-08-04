d6e769228e2d6061ea6fb7cfad076dafcb9eabb8
Miguel P.C <mperezco@redhat.com>
2020-08-04 16:17:35 +0200

    Test existance of files

--- introduction/getting-started-mta/set-env.sh
+++ introduction/getting-started-mta/set-env.sh
@@ -2,5 +2,9 @@
 # This is the script to put provisioning things in, this runs in the foreground
 set -o pipefail 
 
-/var/tmp/deploy.sh /var/tmp/deployment.properties
+while [ ! -f /var/tmp/deploy.sh ]; do sleep 1; done
+while [ ! -f /var/tmp/deployment.properties ]; do sleep 1; done
+echo "Migration Toolkit for Applications Deploying"
+bash  /var/tmp/deploy.sh /var/tmp/deployment.properties
+
 echo "Migration Toolkit for Applications Tutorial Ready!"
