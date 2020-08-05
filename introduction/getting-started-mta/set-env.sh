9eaca1b556fe8a27c5b7c0790005a25bb328d66f
Miguel P.C <mperezco@redhat.com>
2020-08-05 13:04:40 +0200

    fixed assets and commented auto-install

--- introduction/getting-started-mta/set-env.sh
+++ introduction/getting-started-mta/set-env.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 while [ ! -f /var/tmp/deploy.sh ]; do sleep 1; done
 while [ ! -f /var/tmp/deployment.properties ]; do sleep 1; done
-echo "Migration Toolkit for Applications Deploying"
-bash  /var/tmp/deploy.sh /var/tmp/deployment.properties
+#echo "Migration Toolkit for Applications Deploying"
+#bash  /var/tmp/deploy.sh /var/tmp/deployment.properties
 
 echo "Migration Toolkit for Applications Tutorial Ready!"
