7ea5de6a5b43d19e9c709ea6b3ffad17e0c36326
Miguel P.C <mperezco@redhat.com>
2020-08-06 13:14:57 +0200

    Updated profile

--- introduction/getting-started-mta/env-init.sh
+++ introduction/getting-started-mta/env-init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # This is supposed to run in the background to provision your workload, it seems like there are issues with it, but try it out
-while [ ! -f /var/tmp/deploy.sh ]; do sleep 1; done
-while [ ! -f /var/tmp/deployment.properties ]; do sleep 1; done
-bash  /var/tmp/deploy.sh /var/tmp/deployment.properties
+
+#while [ ! -f /var/tmp/deploy.sh ]; do sleep 1; done
+#while [ ! -f /var/tmp/deployment.properties ]; do sleep 1; done
+#bash  /var/tmp/deploy.sh /var/tmp/deployment.properties
 
