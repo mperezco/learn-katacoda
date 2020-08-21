5a44de2656620564105b029bcb8d9b51ccb3c836
Brian Tannous <btannous@redhat.com>
2020-08-21 13:10:33 -0500

    Patch dc for console and executor to remove livenessProbe

--- introduction/getting-started-mta/set-env.sh
+++ introduction/getting-started-mta/set-env.sh
@@ -4,7 +4,8 @@ set -o pipefail
 
 while [ ! -f /var/tmp/deploy.sh ]; do sleep 1; done
 while [ ! -f /var/tmp/deployment.properties ]; do sleep 1; done
-#echo "Migration Toolkit for Applications Deploying"
-#bash  /var/tmp/deploy.sh /var/tmp/deployment.properties
-
+echo "Migration Toolkit for Applications Deploying"
+bash  /var/tmp/deploy.sh /var/tmp/deployment.properties
+oc patch dc mta-web-console --type json -p '[{"op": "remove", "path": "/spec/template/spec/containers/0/livenessProbe"}]'
+oc patch dc mta-web-console-mta-web-console-executor --type json -p '[{"op": "remove", "path": "/spec/template/spec/containers/0/livenessProbe"}]'
 echo "Migration Toolkit for Applications Tutorial Ready!"
