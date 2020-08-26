2a56feb1fb6fca9bb729f2645f6fc063ed94413f
Miguel P.C <mperezco@redhat.com>
2020-08-26 10:59:30 +0200

    Fixed typo in DC modification

--- introduction/getting-started-mta/set-env.sh
+++ introduction/getting-started-mta/set-env.sh
@@ -7,5 +7,5 @@ while [ ! -f /var/tmp/deployment.properties ]; do sleep 1; done
 echo "Migration Toolkit for Applications Deploying"
 bash  /var/tmp/deploy.sh /var/tmp/deployment.properties
 oc patch dc mta-web-console --type json -p '[{"op": "remove", "path": "/spec/template/spec/containers/0/livenessProbe"}]'
-oc patch dc mta-web-console-mta-web-console-executor --type json -p '[{"op": "remove", "path": "/spec/template/spec/containers/0/livenessProbe"}]'
+oc patch dc mta-web-console-executor --type json -p '[{"op": "remove", "path": "/spec/template/spec/containers/0/livenessProbe"}]'
 echo "Migration Toolkit for Applications Tutorial Ready!"
